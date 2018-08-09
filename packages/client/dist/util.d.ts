13c935132ea3177cbc6a7df3b75fdd511c330d05
Daniel <daniel.freese@gmail.com>
2018-08-09 12:37:30 +0200

    added automatic compile (#9)
    * client readme update
    proper package layout
    
    * added discover package and moved the git ignore into the root
    rewritten discover into class syntax
    
    * replaced function() defs with arrow functions
    
    * converted project into typescript
    
    * converted project into typescript
    
    * fixed path issue
    
    * converted discover package into typescript
    fixed bug in client package
    
    * updated packages json for automatic typescript compile

--- packages/client/dist/util.d.ts
+++ packages/client/dist/util.d.ts
@@ -1,6 +1,13 @@
+declare function getUniqueId(): number;
+/**
+ * Splits a response from the hub (usualy seperated by ":" and "=") into a
+ * proper javascript object.
+ */
+declare function decodeColonSeparatedResponse(response: string): Object;
+declare function buildIqStanza(type: string, xmlns: string, mime: string, body: string, from?: string): any;
 declare const _default: {
-    getUniqueId: () => number;
-    decodeColonSeparatedResponse: (response: string) => Object;
-    buildIqStanza: (type: string, xmlns: string, mime: string, body: string, from?: string) => any;
+    getUniqueId: typeof getUniqueId;
+    decodeColonSeparatedResponse: typeof decodeColonSeparatedResponse;
+    buildIqStanza: typeof buildIqStanza;
 };
 export default _default;
